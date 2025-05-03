1295ec183954d4e5c86a17aa6d03cec8ecb70715
DanielV <contactodanielfvp@gmail.com>
2025-05-03 14:55:28 -0500

    creacion del primer estado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import './App.css'
 import { Twitter } from './twitter.jsx'
 
-export function App (){
+export function App (){ 
+
+  const formatArroba = (name) => `@${name}` //componente es una fabrica de elementos
+  // componente = una funcion que al ejecutarse da un elemento
+  // elemento = lo que renderiza react
+
+    // Ej componente (PUEDE SER HTML, LOGICA, ESTILOS, PROPS, ESTADOS):
+    // function Saludo() {
+    //   return <h1>Hola, mundo</h1>;
+    // }
+
+    // function TarjetaUsuario(props) {
+    //   return (
+    //     <div>                     // ← elemento JSX (div)
+    //       <h2>{props.nombre}</h2> // ← elemento JSX (h2)
+    //       <p>Edad: {props.edad}</p> // ← elemento JSX (p)
+    //     </div>
+    //   );
+    // }
+
+    //Consejo 2:
+    // Procurar que las props sean inmutables.
+    
   return(
     <>
-      <Twitter usuario="Daniel Felipe Vargas" name="DanielV"/>
-      <Twitter usuario="Daniel Felipe Vargass" name="DanielVs"/>
-      <Twitter usuario="Daniel Felipe Vargafs" name="DanielVf"/>
+    {/* EN LOS COMPONENTES SE PUEDEN PASAR FUNCIONES, NO SOLO DATOS */}
+      <Twitter arroba={formatArroba} usuario="Daniel Felipe Vargas" name="DanielV"/> 
+      <Twitter arroba={formatArroba} usuario="Daniel Felipe Vargass" name="DanielVs"/>
+      <Twitter arroba={formatArroba} usuario="Daniel Felipe Vargafs" name="DanielVf"/>
     </>
   )
 }
--- src/twitter.jsx
+++ src/twitter.jsx
@@ -1,16 +1,27 @@
-export function Twitter ({usuario, name}){
-    return(
+import { useState } from "react"
+
+export function Twitter ({arroba, usuario, name}){ //se pueden dar valores preseteados, por ejemplo usuario='unknown'
+
+  const [isFollowing, setIsFollowing] = useState(false)
+  const manejarClick = () => {
+    setIsFollowing(!isFollowing)
+  }
+
+  const siguiendo = isFollowing ? 'Siguiendo' : 'Seguir' //If especial
+  const siguiendoClass = isFollowing ? 'tw-seguir tw-siguiendo' : 'tw-seguir'
+  
+  return(
       <article className='tw-card'>
         <header className='tw-card-header'>
           <img className='tw-perfil-img' src="https://unavatar.io/dribbble/omidnikrah" alt="avatar" />
           <div className='tw-header-div'>
-            <strong className='tw-nombrePr'>{name}</strong>
-            <span className='tw-nombreUs'>@{usuario}</span>
+            <strong className='tw-nombrePr'>{usuario}</strong>
+            <span className='tw-nombreUs'>{arroba(name)}</span>
           </div>
         </header>
         <aside>
-          <button className='tw-seguir'>
-            Seguir
+          <button className={siguiendoClass} onClick={manejarClick}>
+          {siguiendo}
           </button>
         </aside>
       </article>
